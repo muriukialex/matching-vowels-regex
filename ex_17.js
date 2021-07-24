26df2b72702443d96a71ae1830982feb6a566649
Alex Muriuki <alexmuriukimaina254@gmail.com>
2021-07-24 11:35:46 +0300

    added a sample vowel checker helper function

---
+++ ex_17.js
@@ -0,0 +1,16 @@
+/* a function that matches if a string starts with a vowel and ends with the same vowel it started with
+   and returns a boolean
+*/
+
+function vowelChecker(mystring){
+    //checks first is the passed argument is a string
+    if(typeof mystring !== "string"){
+        return "passed Argument is not a String";
+    }
+    const regex = /^([aeiou])(\w+)\1$/;
+    //the regex above matches any characteres in character set above
+    //has arbitraty large characters in between
+    //uses backward reference to make start character be the end character
+
+    return regex.test(mystring);
+}
